3b675f550a34213d272f6fc532091dc43e74caf7
Abhay Mishra <abhaymishra72501@gmail.com>
2026-07-10 09:10:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.57%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+
+        int count = 0;
+
+        while(n) {
+
+            if(n%2)
+            count++;
+
+
+
+            n/=2;
+        }
+        return count;
+        
+    }
+};
