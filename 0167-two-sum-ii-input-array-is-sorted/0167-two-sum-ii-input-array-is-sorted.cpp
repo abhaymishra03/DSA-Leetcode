24dcbae92babb1e4e5dc12633531d7f7367e4250
Abhay Mishra <abhaymishra72501@gmail.com>
2026-07-13 10:44:24 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.87%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int st = 0 , end = nums.size()-1;
+        vector<int>ans;
+
+        while(st < end){
+            if(nums[st]+nums[end]==target){
+                ans.push_back(st+1);
+                ans.push_back(end+1);
+                break;
+            } else if (nums[st]+nums[end]>target)
+            end--;
+            else 
+            st++;
+
+        }
+
+
+        return ans;
+        
+    }
+};
