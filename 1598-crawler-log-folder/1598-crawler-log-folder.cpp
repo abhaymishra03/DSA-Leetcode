2c7bd6f9fe8d2fbf362b96ea474cd310aefd9124
Abhay Mishra <abhaymishra72501@gmail.com>
2026-07-23 13:04:15 +0530

    Time: 3 ms (9.75%), Space: 14 MB (82.21%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+
+        int ans = 0;
+
+        for (string log : logs) {
+            if (log == "../") {
+
+                if (ans > 0)
+                    ans--;
+            } else if (log != "./") {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
