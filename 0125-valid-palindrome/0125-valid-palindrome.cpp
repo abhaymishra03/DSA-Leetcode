3c026d54acc0ba0bdd864e2a8eef091803caa8cb
Abhay Mishra <abhaymishra72501@gmail.com>
2026-07-27 20:03:30 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
-       bool isAlphanumeric(char ch){
-    if ((ch>='0' && ch<='9')||
-   (tolower(ch)<='z'&& tolower(ch)>='a')){
-        return true ;
-    }
-    return false;
-}
-    bool isPalindrome(string str) {
- bool isPal=true;
+    bool isPalindrome(string s) {
 
-int st=0,end=str.length()-1;
-while(st<end){
-    if(!isAlphanumeric(str[st])){
-        st++;
-        continue;
-    }
-    if(!isAlphanumeric(str[end])){
-        end--;
-        continue;
-
-    }
-    if(tolower(str[st])!=tolower(str[end])){
-       isPal=false;
-       break;
-        
-        
-        
-        
-
-    }
-    st++;
-    end--;
-}
-return isPal;
-
-        
+        int st = 0, end = s.size() - 1;
+        while (st < end) {
+            if (!isalnum(s[st])) {
+                st++;
+                continue;
+            }
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[st++]) != tolower(s[end--]))
+                return false;
+        }
+        return true;
     }
 };
