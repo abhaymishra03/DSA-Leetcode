c3546884c853984e1eb833af3e14ac6826db379f
Abhay Mishra <abhaymishra72501@gmail.com>
2026-09-04 13:11:52 +0530

    Time: 73 ms (50.17%), Space: 111.2 MB (62.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+
+        unordered_map<int,int>mp;
+
+
+        for(int val : nums) {
+
+            mp[val]++;
+
+            if(mp[val]==2)return true;
+        }
+        return false;
+    }
+};
