5ef7b1c19c6f4c83c293947e49db3af431f4469d
Abhay Mishra <abhaymishra72501@gmail.com>
2026-09-20 11:07:24 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,10 +4,10 @@ public:
 
         int sum = 0;
 
-        for(int i = 1; i <= s.size(); i++) {
+        for(int i = 0; i < s.size(); i++) {
 
-            int rev = 26-(s[i-1]-'a');
-            sum+= rev*i;
+            int rev = 26-(s[i]-'a');
+            sum+= rev*(i+1);
         }
         return sum;
     }
