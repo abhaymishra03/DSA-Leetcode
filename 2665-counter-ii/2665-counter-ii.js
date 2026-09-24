7f78a352dd03b035c35f3af5c36646757e7397de
Abhay Mishra <abhaymishra72501@gmail.com>
2026-09-24 13:32:04 +0530

    Time: 50 ms (55.22%), Space: 55.7 MB (89.3%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,40 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+
+
+    const temp = init;
+
+
+    return {
+
+        increment:()=>{
+            return ++init;
+        },
+        decrement:()=>{
+
+       
+            return --init;
+        },
+        reset:()=>{
+
+            init = temp;
+            return init;
+        },
+
+
+
+    }
+
+    
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
