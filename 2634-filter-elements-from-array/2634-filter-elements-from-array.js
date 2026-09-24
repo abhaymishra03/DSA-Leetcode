9e41d2b3f85894c3a7b79c6773dbdd7af8a7e5d1
Abhay Mishra <abhaymishra72501@gmail.com>
2026-09-24 13:45:55 +0530

    Time: 41 ms (70%), Space: 52.5 MB (93.73%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+
+
+    let newArr =[];
+
+
+    arr.forEach((value,index)=>{
+
+        if(fn(value,index))
+        newArr.push(value);
+    })
+
+    return newArr;
+};
